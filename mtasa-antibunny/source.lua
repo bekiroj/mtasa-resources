5ea5a166eeb105a182d10b8de82e1d6ad3b1e7bc
Bekir Aşkar <56192763+bekiroj@users.noreply.github.com>
2020-12-22 01:33:27 +0300

    Add files via upload

---
+++ mtasa-antibunny/source.lua
@@ -0,0 +1,12 @@
+﻿local state
+function Check()
+    if getPedMoveState(getLocalPlayer()) == "sprint" then
+        state = true
+    elseif state and getPedMoveState(getLocalPlayer()) == "jump" then
+        setTimer(setPedAnimation, 50, 1, getLocalPlayer(), "ped", "FALL_collapse", -1, false, true, false, false)
+    else
+        state = nil
+    end
+end
+
+addEventHandler("onClientPreRender",root,Check)
