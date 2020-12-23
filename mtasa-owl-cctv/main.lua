390c0141b3f03a2b5273686f32c90ad861cb9038
Bekir Aşkar <bekiraskar42@gmail.com>
2020-12-23 05:40:12 +0300

    Update main.lua

--- mtasa-owl-cctv/main.lua
+++ mtasa-owl-cctv/main.lua
@@ -53,19 +53,6 @@ function HideBtns()
 	guiSetVisible(backbt, false)
 end
 
-function dxDrawBorderedText( text, x, y, w, h, color, scale, font, alignX, alignY, clip, wordBreak, postGUI )
-trans = 155
-	dxDrawText ( text, x - 1, y - 1, w - 1, h - 1, tocolor ( 0, 0, 0, trans ), scale, font, alignX, alignY, clip, wordBreak, false )
-	dxDrawText ( text, x + 1, y - 1, w + 1, h - 1, tocolor ( 0, 0, 0, trans ), scale, font, alignX, alignY, clip, wordBreak, false )
-	dxDrawText ( text, x - 1, y + 1, w - 1, h + 1, tocolor ( 0, 0, 0, trans ), scale, font, alignX, alignY, clip, wordBreak, false )
-	dxDrawText ( text, x + 1, y + 1, w + 1, h + 1, tocolor ( 0, 0, 0, trans ), scale, font, alignX, alignY, clip, wordBreak, false )
-	dxDrawText ( text, x - 1, y, w - 1, h, tocolor ( 0, 0, 0, trans ), scale, font, alignX, alignY, clip, wordBreak, false )
-	dxDrawText ( text, x + 1, y, w + 1, h, tocolor ( 0, 0, 0, trans ), scale, font, alignX, alignY, clip, wordBreak, false )
-	dxDrawText ( text, x, y - 1, w, h - 1, tocolor ( 0, 0, 0, trans ), scale, font, alignX, alignY, clip, wordBreak, false )
-	dxDrawText ( text, x, y + 1, w, h + 1, tocolor ( 0, 0, 0, trans ), scale, font, alignX, alignY, clip, wordBreak, false )
-	dxDrawText ( text, x, y, w, h, color, scale, font, alignX, alignY, clip, wordBreak, postGUI )
-end
-
 function roundedRectangle(x, y, w, h, borderColor, bgColor, postGUI)
 	if (x and y and w and h) then
 		if (not borderColor) then
