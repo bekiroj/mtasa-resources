6d762d2bd16b694bbb81cd43829df2442eda27fb
Bekir Aşkar <bekiraskar42@gmail.com>
2020-12-23 08:28:49 +0300

    Update command.lua

--- mtasa-owl-cctv/command.lua
+++ mtasa-owl-cctv/command.lua
@@ -1,25 +1,33 @@
+local alan = createColSphere(1890.3955078125, -1755.38671875, 13.3828125, 2)
+setElementInterior(alan, 0)
+setElementDimension(alan, 0)
+
 addCommandHandler("cctv",
 	function(source, cmd)
 		if getElementData(source, "loggedin") == 1 then
 			if getElementData(source, "faction") == 1 or getElementData(source, "faction") == 6 then
-				if not getElementData(source, "cctv") then
-					lastint = getElementInterior(source)
-					lastdim = getElementDimension(source)
-					setElementInterior(source, 0)
-					setElementDimension(source, 0)
+				if isElementWithinColShape(source, alan) then
+					if not getElementData(source, "cctv") then
+						lastint = getElementInterior(source)
+						lastdim = getElementDimension(source)
+						setElementInterior(source, 0)
+						setElementDimension(source, 0)
 
-					setElementFrozen(source, true)
-					setElementData(source, "cctv", true)
-					triggerClientEvent(source,"ShowBtns",source)
-					outputChatBox("[CCTV] #f9f9f9Arayüzü açtınız, kapatmak için /cctv.", source, 30, 30, 30, true)
+						setElementFrozen(source, true)
+						setElementData(source, "cctv", true)
+						triggerClientEvent(source,"ShowBtns",source)
+						outputChatBox("[CCTV] #f9f9f9Arayüzü açtınız, kapatmak için /cctv.", source, 30, 30, 30, true)
+					else
+						setElementInterior(source, lastint)
+						setElementDimension(source, lastdim)
+
+						triggerClientEvent(source,"HideBtns",source)
+						setElementFrozen(source, false)
+						setElementData(source, "cctv", nil)
+						setCameraTarget(source)
+					end
 				else
-					setElementInterior(source, lastint)
-					setElementDimension(source, lastdim)
-
-					triggerClientEvent(source,"HideBtns",source)
-					setElementFrozen(source, false)
-					setElementData(source, "cctv", nil)
-					setCameraTarget(source)
+					outputChatBox("[CCTV] #f9f9f9Bu işlemi kullanabilmek için geçerli alanda olmanız gerekir.", source, 30, 30, 30, true)
 				end
 			end
 		end
